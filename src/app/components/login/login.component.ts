fc97befd82a9a4c3da5448f06620ea7f82208964
ValentinaCaputo <93424373+ValentinaCaputo@users.noreply.github.com>
2023-05-03 11:57:58 +0200

    change email validator

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -27,7 +27,7 @@ export class LoginComponent implements OnInit {
   ) {
 
     this._loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
+      email: new FormControl('', [Validators.required, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/)]),
       password: new FormControl('', Validators.required)
     });
 
