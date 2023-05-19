69e4b3a4fc6b1861dc3538aa7f2937da990d070e
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-05-19 16:47:33 +0200

    W

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,11 +1,16 @@
-import { ComponentFixture, TestBed,fakeAsync, tick } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 import { SessionService } from 'src/app/services/session/session.service';
 import { ClockCaptchaService } from 'src/app/services/clock-captcha/clock-captcha.service';
-import { ClockCAPTCHAView } from '../../../../../clock-captcha/dist/front-end/ClockCAPTCHA';
+import { ClockCAPTCHAView } from '../../../../../clock-captcha/dist/index';
 import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
@@ -18,15 +23,20 @@ describe('LoginComponent', () => {
   let captchaModuleMock: ClockCAPTCHAView;
   let getElementByIdSpy: jasmine.Spy;
 
-
   beforeEach(() => {
     //crea tutti i mock necessari
     routerMock = jasmine.createSpyObj('Router', ['navigate']);
     snackBarMock = jasmine.createSpyObj('MatSnackBar', ['open']);
     sessionServiceMock = jasmine.createSpyObj('SessionService', ['login']);
     ccServiceMock = jasmine.createSpyObj('ClockCaptchaService', ['ccInit']);
-    captchaModuleMock = jasmine.createSpyObj('ClockCAPTCHAView', ['inject', 'fill','getInput', 'getToken','error','clear']);
-
+    captchaModuleMock = jasmine.createSpyObj('ClockCAPTCHAView', [
+      'inject',
+      'fill',
+      'getInput',
+      'getToken',
+      'error',
+      'clear',
+    ]);
 
     TestBed.configureTestingModule({
       providers: [
@@ -35,8 +45,8 @@ describe('LoginComponent', () => {
         { provide: MatSnackBar, useValue: snackBarMock },
         { provide: SessionService, useValue: sessionServiceMock },
         { provide: ClockCaptchaService, useValue: ccServiceMock },
-        { provide: ClockCAPTCHAView, useValue: captchaModuleMock }
-      ]
+        { provide: ClockCAPTCHAView, useValue: captchaModuleMock },
+      ],
     });
 
     //component = new LoginComponent(routerMock, snackBarMock, sessionServiceMock, ccServiceMock);
@@ -47,9 +57,12 @@ describe('LoginComponent', () => {
     routerMock = TestBed.inject(Router);
     snackBarMock = TestBed.inject(MatSnackBar);
     sessionServiceMock = TestBed.inject(SessionService);
-    ccServiceMock.ccInit = jasmine.createSpy().and.returnValue(of({ cc_content: 'content', cc_token: 'token' }));
-    getElementByIdSpy = spyOn(document, 'getElementById').and.returnValue(document.createElement('div'));
-
+    ccServiceMock.ccInit = jasmine
+      .createSpy()
+      .and.returnValue(of({ cc_content: 'content', cc_token: 'token' }));
+    getElementByIdSpy = spyOn(document, 'getElementById').and.returnValue(
+      document.createElement('div')
+    );
   });
 
   //Test costruttore
@@ -72,7 +85,9 @@ describe('LoginComponent', () => {
     component.ngOnInit();
 
     expect(getElementByIdSpy).toHaveBeenCalledWith('clock-captcha');
-    expect(captchaModuleMock.inject).toHaveBeenCalledWith(jasmine.any(HTMLElement));
+    expect(captchaModuleMock.inject).toHaveBeenCalledWith(
+      jasmine.any(HTMLElement)
+    );
 
     expect(component['_captchaModule']).toBeDefined();
 
@@ -80,8 +95,10 @@ describe('LoginComponent', () => {
     tick(2000);
 
     expect(ccServiceMock.ccInit).toHaveBeenCalled();
-    expect(component['_captchaModule'].fill).toHaveBeenCalledWith('content', 'token');
-
+    expect(component['_captchaModule'].fill).toHaveBeenCalledWith(
+      'content',
+      'token'
+    );
   }));
 
   //login
@@ -91,7 +108,9 @@ describe('LoginComponent', () => {
     component.login();
 
     expect(captchaModuleMock.getInput).toHaveBeenCalled();
-    expect(captchaModuleMock.error).toHaveBeenCalledWith("Controlla il formato!");
+    expect(captchaModuleMock.error).toHaveBeenCalledWith(
+      'Controlla il formato!'
+    );
   });
 
   it('should navigate to home page on successful login', () => {
@@ -100,16 +119,20 @@ describe('LoginComponent', () => {
 
     const loginForm = new FormGroup({
       email: new FormControl(),
-      password: new FormControl()
+      password: new FormControl(),
     });
 
     loginForm.setValue(loginFormValue);
     component['_loginForm'] = loginForm;
 
     captchaModuleMock.getInput = jasmine.createSpy().and.returnValue('12345');
-    captchaModuleMock.getToken = jasmine.createSpy().and.returnValue('token')
+    captchaModuleMock.getToken = jasmine.createSpy().and.returnValue('token');
 
-    sessionServiceMock.login = jasmine.createSpy().and.returnValue({ subscribe: (callback: Function) => callback(loginResult) });
+    sessionServiceMock.login = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: Function) => callback(loginResult),
+      });
 
     component.login();
 
@@ -122,32 +145,54 @@ describe('LoginComponent', () => {
     expect(routerMock.navigate).toHaveBeenCalledWith(['']);
   });
 
-
   it('should handle BAD_CAPTCHA error case', () => {
     const loginFormValue = { email: 'test@example.com', password: 'password' };
     const loginForm = new FormGroup({
       email: new FormControl(),
-      password: new FormControl()
+      password: new FormControl(),
     });
     loginForm.setValue(loginFormValue);
     component['_loginForm'] = loginForm;
 
     captchaModuleMock.getInput = jasmine.createSpy().and.returnValue('12345');
-    captchaModuleMock.getToken = jasmine.createSpy().and.returnValue('captcha-token')
+    captchaModuleMock.getToken = jasmine
+      .createSpy()
+      .and.returnValue('captcha-token');
 
     const loginResponse = { okay: false, case: 'BAD_CAPTCHA' };
-    const ccServiceResponse = { cc_content: 'captcha-content', cc_token: 'captcha-token' };
+    const ccServiceResponse = {
+      cc_content: 'captcha-content',
+      cc_token: 'captcha-token',
+    };
 
-    sessionServiceMock.login = jasmine.createSpy().and.returnValue({ subscribe: (callback: any) => callback(loginResponse) });
-    ccServiceMock.ccInit = jasmine.createSpy().and.returnValue({ subscribe: (callback: any) => callback(ccServiceResponse) });
+    sessionServiceMock.login = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: any) => callback(loginResponse),
+      });
+    ccServiceMock.ccInit = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: any) => callback(ccServiceResponse),
+      });
 
     component.login();
 
     expect(captchaModuleMock.clear).toHaveBeenCalled();
-    expect(captchaModuleMock.error).toHaveBeenCalledWith('OPS, ORARIO SCORRETTO!');
+    expect(captchaModuleMock.error).toHaveBeenCalledWith(
+      'OPS, ORARIO SCORRETTO!'
+    );
     expect(ccServiceMock.ccInit).toHaveBeenCalled();
-    expect(captchaModuleMock.fill).toHaveBeenCalledWith('captcha-content', 'captcha-token');
-    expect(sessionServiceMock.login).toHaveBeenCalledWith('test@example.com', 'password', 'captcha-token', '12345');
+    expect(captchaModuleMock.fill).toHaveBeenCalledWith(
+      'captcha-content',
+      'captcha-token'
+    );
+    expect(sessionServiceMock.login).toHaveBeenCalledWith(
+      'test@example.com',
+      'password',
+      'captcha-token',
+      '12345'
+    );
     expect(routerMock.navigate).not.toHaveBeenCalled();
   });
 
@@ -155,22 +200,37 @@ describe('LoginComponent', () => {
     const loginFormValue = { email: 'test@example.com', password: 'password' };
     const loginForm = new FormGroup({
       email: new FormControl(),
-      password: new FormControl()
+      password: new FormControl(),
     });
     loginForm.setValue(loginFormValue);
     component['_loginForm'] = loginForm;
 
     captchaModuleMock.getInput = jasmine.createSpy().and.returnValue('12345');
-    captchaModuleMock.getToken = jasmine.createSpy().and.returnValue('captcha-token')
+    captchaModuleMock.getToken = jasmine
+      .createSpy()
+      .and.returnValue('captcha-token');
 
-    const ccServiceResponse = { cc_content: 'captcha-content', cc_token: 'captcha-token' };
+    const ccServiceResponse = {
+      cc_content: 'captcha-content',
+      cc_token: 'captcha-token',
+    };
     const loginResponse = { okay: false, case: 'INVALID_EMAIL_FORMAT' };
 
-    ccServiceMock.ccInit = jasmine.createSpy().and.returnValue({ subscribe: (callback: any) => callback(ccServiceResponse) });
-    sessionServiceMock.login = jasmine.createSpy().and.returnValue({ subscribe: (callback: any) => callback(loginResponse) });
+    ccServiceMock.ccInit = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: any) => callback(ccServiceResponse),
+      });
+    sessionServiceMock.login = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: any) => callback(loginResponse),
+      });
 
-    const emailControl: AbstractControl<any, any> = component['_loginForm'].get('email')!;
-    const passwordControl: AbstractControl<any, any> = component['_loginForm'].get('password')!;
+    const emailControl: AbstractControl<any, any> =
+      component['_loginForm'].get('email')!;
+    const passwordControl: AbstractControl<any, any> =
+      component['_loginForm'].get('password')!;
 
     spyOn(emailControl, 'setErrors');
     spyOn(passwordControl, 'setErrors');
@@ -178,34 +238,61 @@ describe('LoginComponent', () => {
     component.login();
 
     expect(ccServiceMock.ccInit).toHaveBeenCalled();
-    expect(sessionServiceMock.login).toHaveBeenCalledWith('test@example.com', 'password', 'captcha-token', '12345');
+    expect(sessionServiceMock.login).toHaveBeenCalledWith(
+      'test@example.com',
+      'password',
+      'captcha-token',
+      '12345'
+    );
     expect(routerMock.navigate).not.toHaveBeenCalled();
     expect(captchaModuleMock.clear).toHaveBeenCalled();
-    expect(captchaModuleMock.fill).toHaveBeenCalledWith('captcha-content', 'captcha-token');
-    expect(emailControl.setErrors).toHaveBeenCalledWith({ wrongCredentialError: true });
-    expect(passwordControl.setErrors).toHaveBeenCalledWith({ wrongCredentialError: true });
+    expect(captchaModuleMock.fill).toHaveBeenCalledWith(
+      'captcha-content',
+      'captcha-token'
+    );
+    expect(emailControl.setErrors).toHaveBeenCalledWith({
+      wrongCredentialError: true,
+    });
+    expect(passwordControl.setErrors).toHaveBeenCalledWith({
+      wrongCredentialError: true,
+    });
   });
 
   it('should handle BAD_CREDENTIAL error case', () => {
     const loginFormValue = { email: 'test@example.com', password: 'password' };
     const loginForm = new FormGroup({
       email: new FormControl(),
-      password: new FormControl()
+      password: new FormControl(),
     });
     loginForm.setValue(loginFormValue);
     component['_loginForm'] = loginForm;
 
     captchaModuleMock.getInput = jasmine.createSpy().and.returnValue('12345');
-    captchaModuleMock.getToken = jasmine.createSpy().and.returnValue('captcha-token')
+    captchaModuleMock.getToken = jasmine
+      .createSpy()
+      .and.returnValue('captcha-token');
 
-    const ccServiceResponse = { cc_content: 'captcha-content', cc_token: 'captcha-token' };
+    const ccServiceResponse = {
+      cc_content: 'captcha-content',
+      cc_token: 'captcha-token',
+    };
     const loginResponse = { okay: false, case: 'BAD_CREDENTIAL' };
 
-    ccServiceMock.ccInit = jasmine.createSpy().and.returnValue({ subscribe: (callback: any) => callback(ccServiceResponse) });
-    sessionServiceMock.login = jasmine.createSpy().and.returnValue({ subscribe: (callback: any) => callback(loginResponse) });
+    ccServiceMock.ccInit = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: any) => callback(ccServiceResponse),
+      });
+    sessionServiceMock.login = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: any) => callback(loginResponse),
+      });
 
-    const emailControl: AbstractControl<any, any> = component['_loginForm'].get('email')!;
-    const passwordControl: AbstractControl<any, any> = component['_loginForm'].get('password')!;
+    const emailControl: AbstractControl<any, any> =
+      component['_loginForm'].get('email')!;
+    const passwordControl: AbstractControl<any, any> =
+      component['_loginForm'].get('password')!;
 
     spyOn(emailControl, 'setErrors');
     spyOn(passwordControl, 'setErrors');
@@ -213,40 +300,75 @@ describe('LoginComponent', () => {
     component.login();
 
     expect(ccServiceMock.ccInit).toHaveBeenCalled();
-    expect(sessionServiceMock.login).toHaveBeenCalledWith('test@example.com', 'password', 'captcha-token', '12345');
+    expect(sessionServiceMock.login).toHaveBeenCalledWith(
+      'test@example.com',
+      'password',
+      'captcha-token',
+      '12345'
+    );
     expect(routerMock.navigate).not.toHaveBeenCalled();
     expect(captchaModuleMock.clear).toHaveBeenCalled();
-    expect(captchaModuleMock.fill).toHaveBeenCalledWith('captcha-content', 'captcha-token');
-    expect(emailControl.setErrors).toHaveBeenCalledWith({ wrongCredentialError: true });
-    expect(passwordControl.setErrors).toHaveBeenCalledWith({ wrongCredentialError: true });
+    expect(captchaModuleMock.fill).toHaveBeenCalledWith(
+      'captcha-content',
+      'captcha-token'
+    );
+    expect(emailControl.setErrors).toHaveBeenCalledWith({
+      wrongCredentialError: true,
+    });
+    expect(passwordControl.setErrors).toHaveBeenCalledWith({
+      wrongCredentialError: true,
+    });
   });
 
   it('should handle default error case', () => {
     const loginFormValue = { email: 'test@example.com', password: 'password' };
     const loginForm = new FormGroup({
       email: new FormControl(),
-      password: new FormControl()
+      password: new FormControl(),
     });
     loginForm.setValue(loginFormValue);
     component['_loginForm'] = loginForm;
 
     captchaModuleMock.getInput = jasmine.createSpy().and.returnValue('12345');
-    captchaModuleMock.getToken = jasmine.createSpy().and.returnValue('captcha-token')
+    captchaModuleMock.getToken = jasmine
+      .createSpy()
+      .and.returnValue('captcha-token');
 
-    const ccServiceResponse = { cc_content: 'captcha-content', cc_token: 'captcha-token' };
+    const ccServiceResponse = {
+      cc_content: 'captcha-content',
+      cc_token: 'captcha-token',
+    };
     const loginResponse = { okay: false, case: 'Some-Error' };
 
-    ccServiceMock.ccInit = jasmine.createSpy().and.returnValue({ subscribe: (callback: any) => callback(ccServiceResponse) });
-    sessionServiceMock.login = jasmine.createSpy().and.returnValue({ subscribe: (callback: any) => callback(loginResponse) });
+    ccServiceMock.ccInit = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: any) => callback(ccServiceResponse),
+      });
+    sessionServiceMock.login = jasmine
+      .createSpy()
+      .and.returnValue({
+        subscribe: (callback: any) => callback(loginResponse),
+      });
 
     component.login();
 
     expect(ccServiceMock.ccInit).toHaveBeenCalled();
-    expect(sessionServiceMock.login).toHaveBeenCalledWith('test@example.com', 'password', 'captcha-token', '12345');
+    expect(sessionServiceMock.login).toHaveBeenCalledWith(
+      'test@example.com',
+      'password',
+      'captcha-token',
+      '12345'
+    );
     expect(routerMock.navigate).not.toHaveBeenCalled();
     expect(captchaModuleMock.clear).toHaveBeenCalled();
-    expect(captchaModuleMock.fill).toHaveBeenCalledWith('captcha-content', 'captcha-token');
-    expect(snackBarMock.open).toHaveBeenCalledWith("Errore interno al sito. Riprova tra qualche minuto.", 'Ok');
+    expect(captchaModuleMock.fill).toHaveBeenCalledWith(
+      'captcha-content',
+      'captcha-token'
+    );
+    expect(snackBarMock.open).toHaveBeenCalledWith(
+      'Errore interno al sito. Riprova tra qualche minuto.',
+      'Ok'
+    );
   });
-
 });
