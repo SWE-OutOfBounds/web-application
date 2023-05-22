287a638256e5b11b66e9815fc40d5a41811b7544
ValentinaCaputo <93424373+ValentinaCaputo@users.noreply.github.com>
2023-05-22 20:04:53 +0200

    cypress configuration

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,5 @@
+describe('The Login Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/login'); // change URL to match your dev URL
+  });
+});
---
+++ cypress/e2e/registration.cy.ts
@@ -0,0 +1,5 @@
+describe('The Registration Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/signup'); // change URL to match your dev URL
+  });
+});
